87f82d49046ce58aeadb3c1e0d5bad38e7566197
Mika Hänninen <mika@robocorp.com>
2020-04-17 18:45:20 +0300

    Add missing docstring to keyword in RPA.Database

--- src/RPA/Database.py
+++ src/RPA/Database.py
@@ -18,4 +18,9 @@ class Database(DatabaseLibrary):
         DatabaseLibrary.__init__(self, *args, **kwargs)
 
     def database_query_result_as_table(self, query_string):
+        """Make database query and return result as `RPA.Table`.
+
+        :param query_string: SQL query string
+        :return: query result as `RPA.Table`
+        """
         return Table(self.query(query_string, returnAsDict=True))
