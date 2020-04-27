038c18333d16f4c749fa79a772f82f301d7d8342
Mika Hänninen <mika@robocorp.com>
2020-04-27 22:20:17 +0300

    PDF: Add keywords and typehinting

--- src/RPA/PDF.py
+++ src/RPA/PDF.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from fpdf import FPDF, HTMLMixin
 import PyPDF2
+from PyPDF2.xmp import XmpInformation
 
 from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
 from RPA.core.utils import UNDEFINED
@@ -18,7 +19,7 @@ class PDF(FPDF, HTMLMixin):
     """RPA Framework library for PDF management.
     """
 
-    def __init__(self, outdir="."):
+    def __init__(self, outdir: str = ".") -> None:
         FPDF.__init__(self)
         HTMLMixin.__init__(self)
         self.logger = logging.getLogger(__name__)
@@ -27,36 +28,63 @@ class PDF(FPDF, HTMLMixin):
         listener = RobotLogListener()
         listener.register_protected_keywords(["RPA.PDF.decrypt"])
 
-    def set_output_directory(self, outdir="."):
+    def set_output_directory(self, outdir: str = ".") -> None:
         """Set output directory where target files are saved to.
 
         :param outdir: output directory path, default to current directory
         """
         self.output_directory = Path(outdir)
 
-    def get_output_directory(self):
+    def get_output_directory(self) -> str:
         """Get output directory where target files are saved to.
 
         :return: absolute filepath as string
         """
         return str(self.output_directory)
 
-    def set_source_document(self, source_pdf):
+    def set_source_document(self, source_pdf: str = None) -> None:
         """Set source PDF document for further operations.
 
         :param source_pdf: filepath to the source pdf
         """
         self.source_pdf = source_pdf
 
-    def add_pages(self, pages=1):
+    def add_pages(self, pages: int = 1) -> None:
         """Adds pages into PDF documents.
 
         :param pages: number of pages to add, defaults to 1
         """
-        for _ in range(pages):
+        for _ in range(int(pages)):
             self.add_page()
 
-    def template_html_to_pdf(self, template, filename, variables=None):
+    def add_pages_to_source_document(
+        self, pages: int = 1, source_pdf: str = None, target_pdf: str = None
+    ) -> None:
+        """Add pages into current source document
+
+        :param pages: number of pages to add, defaults to 1
+        :param source_pdf: filepath to the source pdf
+        :param target_pdf: filename to the target pdf, stored by default
+            to `output_directory`
+        """
+        source_pdf = self._validate(source_pdf, target_pdf)
+        source_reader = PyPDF2.PdfFileReader(open(source_pdf, "rb"))
+        source_page = source_reader.getPage(0)
+
+        pdf_writer = PyPDF2.PdfFileWriter()
+        output_filepath = Path(self.output_directory / target_pdf)
+        pageobject = PyPDF2.pdf.PageObject.createBlankPage(
+            None, source_page.mediaBox.getWidth(), source_page.mediaBox.getHeight()
+        )
+        pdf_writer.appendPagesFromReader(source_reader)
+        for pageno in range(int(pages)):
+            pdf_writer.addPage(pageobject)
+        with open(output_filepath, "wb") as f:
+            pdf_writer.write(f)
+
+    def template_html_to_pdf(
+        self, template: str, filename: str, variables: dict = None
+    ) -> None:
         """Use HTML template file to generate PDF file.
 
         :param template: filepath to HTML template
@@ -76,7 +104,7 @@ class PDF(FPDF, HTMLMixin):
         self.output(self.output_directory / filename)
         self.__init__()
 
-    def get_info(self, source_pdf=None):
+    def get_info(self, source_pdf: str = None) -> dict:
         """Get information from PDF document.
 
         :param source_pdf: filepath to the source pdf
@@ -95,7 +123,9 @@ class PDF(FPDF, HTMLMixin):
         page_structure["numberOfPages"] = pdf_reader.getNumPages()
         return page_structure
 
-    def extract_pages_from_pdf(self, pages, source_pdf=None, target_pdf=None):
+    def extract_pages_from_pdf(
+        self, pages: int, source_pdf: str = None, target_pdf: str = None
+    ) -> None:
         """Extract pages from source PDF and save to target PDF document.
 
         :param source_pdf: filepath to the source pdf
@@ -112,11 +142,11 @@ class PDF(FPDF, HTMLMixin):
             pagelist = pages
         pdf_reader = PyPDF2.PdfFileReader(source_pdf)
         for page in pagelist:
-            pdf_writer.addPage(pdf_reader.getPage(page))
+            pdf_writer.addPage(pdf_reader.getPage(int(page)))
         with open(str(output_filepath), "wb") as f:
             pdf_writer.write(f)
 
-    def get_text_from_pdf(self, pages, source_pdf=None):
+    def get_text_from_pdf(self, pages: int, source_pdf: str = None) -> dict:
         """Get text from set of pages in source PDF document.
 
         :param source_pdf: filepath to the source pdf
@@ -131,11 +161,11 @@ class PDF(FPDF, HTMLMixin):
         else:
             pagelist = pages
         for page in pagelist:
-            page_obj = pdf_reader.getPage(page)
+            page_obj = pdf_reader.getPage(int(page))
             texts[page] = page_obj.extractText()
         return texts
 
-    def _validate(self, source=UNDEFINED, target=UNDEFINED):
+    def _validate(self, source: str = UNDEFINED, target: str = UNDEFINED) -> str:
         if target is not UNDEFINED:
             if target is None:
                 raise ValueError("Target filepath is missing.")
@@ -147,8 +177,13 @@ class PDF(FPDF, HTMLMixin):
         return source
 
     def page_rotate(
-        self, pages, source_pdf=None, target_pdf=None, clockwise=True, angle=90
-    ):
+        self,
+        pages: int,
+        source_pdf: str = None,
+        target_pdf: str = None,
+        clockwise: bool = True,
+        angle: int = 90,
+    ) -> None:
         """Rotate pages in source PDF document and save to target PDF document.
 
         :param source_pdf: filepath to the source pdf
@@ -167,19 +202,19 @@ class PDF(FPDF, HTMLMixin):
         else:
             pagelist = pages
         for page in range(pdf_reader.getNumPages()):
-            source_page = pdf_reader.getPage(page)
+            source_page = pdf_reader.getPage(int(page))
             if page in pagelist:
                 if clockwise:
-                    source_page.rotateClockwise(angle)
+                    source_page.rotateClockwise(int(angle))
                 else:
-                    source_page.rotateCounterClockwise(angle)
+                    source_page.rotateCounterClockwise(int(angle))
             else:
-                source_page = pdf_reader.getPage(page)
+                source_page = pdf_reader.getPage(int(page))
             pdf_writer.addPage(source_page)
         with open(str(output_filepath), "wb") as fh:
             pdf_writer.write(fh)
 
-    def is_pdf_encrypted(self, source_pdf=None):
+    def is_pdf_encrypted(self, source_pdf: str = None) -> bool:
         """Check if PDF is encrypted.
 
         Returns True even if PDF was decrypted.
@@ -188,9 +223,30 @@ class PDF(FPDF, HTMLMixin):
         :return: True if file is encrypted
         """
         source_pdf = self._validate(source_pdf)
-        return PyPDF2.PdfFileReader(source_pdf).isEncrypted()
+        return PyPDF2.PdfFileReader(source_pdf).isEncrypted
 
-    def pdf_decrypt(self, source_pdf=None, password=None):
+    def pdf_encrypt(
+        self,
+        source_pdf: str = None,
+        target_pdf: str = None,
+        user_pwd: str = "",
+        owner_pwd: str = None,
+        use_128bit: bool = True,
+    ):
+        """
+        """
+        source_pdf = self._validate(source_pdf, target_pdf)
+        output_filepath = Path(self.output_directory / target_pdf)
+        if owner_pwd is None:
+            owner_pwd = user_pwd
+        pdf_reader = PyPDF2.PdfFileReader(source_pdf)
+        pdf_writer = PyPDF2.PdfFileWriter()
+        pdf_writer.appendPagesFromReader(pdf_reader)
+        pdf_writer.encrypt(user_pwd, owner_pwd, use_128bit)
+        with open(str(output_filepath), "wb") as f:
+            pdf_writer.write(f)
+
+    def pdf_decrypt(self, source_pdf: str = None, password: str = None) -> bool:
         """Decrypt PDF with password.
 
         :param source_pdf: filepath to the source pdf
@@ -213,9 +269,18 @@ class PDF(FPDF, HTMLMixin):
             raise ValueError(
                 f"Document {source_pdf} uses an unsupported encryption method."
             )
+        except KeyError:
+            self.logger.info("PDF is not encrypted")
+            return False
         return False
 
-    def get_fields(self, source_pdf=None, target_file=None, tree=None, retval=None):
+    def get_fields(
+        self,
+        source_pdf: str = None,
+        target_file: str = None,
+        tree: str = None,
+        retval: str = None,
+    ) -> dict:
         """Get interactive form fields from PDF source and store into target
         file in text format.
 
@@ -230,16 +295,22 @@ class PDF(FPDF, HTMLMixin):
             tree=tree, retval=retval, fileobj=target_file
         )
 
-    def get_form_text_fields(self, source_pdf=None):
+    def get_form_text_fields(self, source_pdf: str = None) -> dict:
         """Get form text fields with textual data (inputs, dropdowns).
 
         :param source_pdf: filepath to the source pdf
         :return: dict of fields or None
         """
         source_pdf = self._validate(source_pdf)
-        return PyPDF2.PdfFileReader(source_pdf).getFormTextFields()
+        try:
+            fields = PyPDF2.PdfFileReader(source_pdf).getFormTextFields()
+        except TypeError:
+            return None
+        return fields
 
-    def get_outlines(self, source_pdf=None, node=None, outlines=None):
+    def get_outlines(
+        self, source_pdf: str = None, node: str = None, outlines: str = None
+    ) -> list:
         """Get document outline.
 
         :param source_pdf: filepath to the source pdf
@@ -250,7 +321,7 @@ class PDF(FPDF, HTMLMixin):
         source_pdf = self._validate(source_pdf)
         return PyPDF2.PdfFileReader(source_pdf).getOutlines(node, outlines)
 
-    def get_page_layout(self, source_pdf=None):
+    def get_page_layout(self, source_pdf: str = None) -> str:
         """Get page layout setting.
 
         :param source_pdf: filepath to the source pdf
@@ -259,7 +330,7 @@ class PDF(FPDF, HTMLMixin):
         source_pdf = self._validate(source_pdf)
         return PyPDF2.PdfFileReader(source_pdf).getPageLayout()
 
-    def get_page_mode(self, source_pdf=None):
+    def get_page_mode(self, source_pdf: str = None) -> str:
         """Get page mode setting.
 
         :param source_pdf: filepath to the source pdf
@@ -268,7 +339,7 @@ class PDF(FPDF, HTMLMixin):
         source_pdf = self._validate(source_pdf)
         return PyPDF2.PdfFileReader(source_pdf).getPageMode()
 
-    def get_xmp_metadata(self, source_pdf=None):
+    def get_xmp_metadata(self, source_pdf: str = None) -> XmpInformation:
         """Get document XMP (Extensible Metadata Platform) data.
 
         :param source_pdf: filepath to the source pdf
@@ -277,7 +348,9 @@ class PDF(FPDF, HTMLMixin):
         source_pdf = self._validate(source_pdf)
         return PyPDF2.PdfFileReader(source_pdf).getXmpMetadata()
 
-    def get_named_destinations(self, source_pdf=None, tree=None, retval=None):
+    def get_named_destinations(
+        self, source_pdf: str = None, tree: str = None, retval: str = None
+    ) -> dict:
         """Get named destination in the document.
 
         :param source_pdf: filepath to the source pdf
@@ -290,7 +363,7 @@ class PDF(FPDF, HTMLMixin):
             tree=tree, retval=retval
         )
 
-    def get_number_of_pages(self, source_pdf=None):
+    def get_number_of_pages(self, source_pdf: str = None) -> int:
         """Get number of pages in the document.
 
         :param source_pdf: filepath to the source pdf
