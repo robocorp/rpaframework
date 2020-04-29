2def93b94319d43b61dc0c54e4758e8c1804cfd0
Mika Hänninen <mika@robocorp.com>
2020-04-29 09:58:30 +0300

    Fix: Correct link into HTTP docstring

--- src/RPA/HTTP.py
+++ src/RPA/HTTP.py
@@ -3,7 +3,7 @@ from RequestsLibrary import RequestsLibrary
 
 class HTTP(RequestsLibrary):
     """RPA Framework HTTP library which wraps
-    `RequestsLibrary <https://hub.robocorp.com/libraries/bulkan-robotframework-requests/>`_ functionality.
+    `RequestsLibrary <https://github.com/MarketSquare/robotframework-requests/>`_ functionality.
     """  # noqa: E501; pylint: disable=line-too-long
 
     def __init__(self, *args, **kwargs) -> None:
