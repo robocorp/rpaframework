239b1476ad17f4500bee5d074bb4e5bcafa4d4bb
Mika Hänninen <mika@robocorp.com>
2020-05-05 10:33:08 +0300

    HTTP: Keyword 'Download' changed to always save content

--- src/RPA/HTTP.py
+++ src/RPA/HTTP.py
@@ -1,13 +1,15 @@
 import logging
 from urllib.parse import urlparse
+from typing import Any
+
 from RequestsLibrary import RequestsLibrary
 from RPA.FileSystem import FileSystem
 
 
 class HTTP(RequestsLibrary):
     """RPA Framework HTTP library which wraps
-    `RequestsLibrary <https://github.com/MarketSquare/robotframework-requests/>`_ functionality.
-    """  # noqa: E501; pylint: disable=line-too-long
+    _RequestsLibrary_ functionality.
+    """
 
     def __init__(self, *args, **kwargs) -> None:
         RequestsLibrary.__init__(self, *args, **kwargs)
@@ -23,6 +25,7 @@ class HTTP(RequestsLibrary):
         binary: bool = True,
         verify: bool = True,
         force_new_session: bool = False,
+        overwrite: bool = False,
     ) -> dict:
         """
         Helper method for `Get Request` which will create session and
@@ -31,11 +34,13 @@ class HTTP(RequestsLibrary):
         Old session will be used if URL scheme and host are same as previously,
         eg. 'https://www.google.fi' part of the URL.
 
-        :param url: target url for Get requesdt
+        :param url: target url for Get request
         :param target_file: filepath to save request content, default `None`
         :param binary: if `True` file is saved as binary, default `True`
         :param verify: if SSL verification should be done, default `True`
         :param force_new_session: if new HTTP session should be created, default `False`
+        :param overwrite: used together with `target_file`, if `True` will
+            overwrite the target file
         :return: request response
         """
         uc = urlparse(url)
@@ -51,11 +56,18 @@ class HTTP(RequestsLibrary):
         self.current_session_alias = request_alias
         response = self.get_request(request_alias, url_path)
         if target_file is not None:
-            if binary:
-                self.fs.create_binary_file(target_file, response.content)
-            else:
-                self.fs.create_file(target_file, response.content)
+            self._create_or_overwrite_target_file(
+                target_file, response.content, binary, overwrite
+            )
         return response
+
+    def _create_or_overwrite_target_file(
+        self, target_file: str, content: Any, binary: bool, overwrite: bool,
+    ) -> None:
+        if binary:
+            self.fs.create_binary_file(target_file, content, overwrite)
+        else:
+            self.fs.create_file(target_file, content, overwrite)
 
     def get_current_session_alias(self) -> str:
         """Get request session alias which was used with `HTTP Get` keyword.
@@ -71,6 +83,24 @@ class HTTP(RequestsLibrary):
         binary: bool = True,
         verify: bool = True,
         force_new_session: bool = False,
+        overwrite: bool = False,
     ) -> dict:
-        """Alias for keyword http_get"""
-        return self.http_get(url, target_file, binary, verify, force_new_session)
+        """Alias for keyword `HTTP Get`.
+
+        Difference in use is that URL is always downloaded based on
+        the URL path (even without `target_file`). If there is a filename
+        in the path then that is used as `target_file` to save to. By default
+        filename will be `downloaded.html`.
+        """
+        response = self.http_get(
+            url, target_file, binary, verify, force_new_session, overwrite
+        )
+        if target_file is None:
+            uc = urlparse(url)
+            target = uc.path.rsplit("/", 1)[-1]
+            if not target:
+                target = "downloaded.html"
+
+            self._create_or_overwrite_target_file(
+                target, response.content, binary, overwrite
+            )
