0fd855c771439d3b1433215c1f45d106e7e6d3ad
Mika Hänninen <mika@robocorp.com>
2020-06-26 14:10:50 +0300

    exchange: add keywords for folder management

--- packages/main/src/RPA/Email/Exchange.py
+++ packages/main/src/RPA/Email/Exchange.py
@@ -1,6 +1,16 @@
-from pathlib import Path
 import logging
-from exchangelib import Account, Credentials, FileAttachment, HTMLBody, Mailbox, Message
+from pathlib import Path
+from typing import Any
+
+from exchangelib import (
+    Account,
+    Credentials,
+    FileAttachment,
+    Folder,
+    HTMLBody,
+    Mailbox,
+    Message,
+)
 
 
 class Exchange:
@@ -26,7 +36,7 @@ class Exchange:
             username, credentials=self.credentials, autodiscover=autodiscover
         )
 
-    def list_messages(self, count: int = 100) -> list:
+    def list_messages(self, count: int = 100, folder_name: str = None) -> list:
         """List messages in the account inbox. Order by descending
         received time.
 
@@ -34,7 +44,8 @@ class Exchange:
         """
         # pylint: disable=no-member
         messages = []
-        for item in self.account.inbox.all().order_by("-datetime_received")[:count]:
+        items = self._get_all_items_in_folder(folder_name)
+        for item in items.order_by("-datetime_received")[:count]:
             messages.append(
                 {
                     "subject": item.subject,
@@ -43,6 +54,13 @@ class Exchange:
                 }
             )
         return messages
+
+    def _get_all_items_in_folder(self, folder_name=None, parent_folder=None) -> list:
+        if parent_folder is None or parent_folder == self.account.inbox:
+            target_folder = self.account.inbox / folder_name
+        else:
+            target_folder = self.account.inbox / parent_folder / folder_name
+        return target_folder.all()
 
     def send_message(
         self,
@@ -155,3 +173,89 @@ class Exchange:
                 msg.attach(fileat)
                 if html:
                     body = body.replace(imname, f"cid:{imname}")
+
+    def create_folder(self, folder_name: str = None, parent_folder: str = None) -> bool:
+        """Create email folder
+
+        :param folder_name: name for the new folder
+        :param parent_folder: name for the parent folder, by default INBOX
+        :return: True if operation was successful, False if not
+        """
+        if folder_name is None:
+            raise KeyError("'folder_name' is required for create folder")
+        if parent_folder is None or parent_folder == self.account.inbox:
+            parent = self.account.inbox
+        else:
+            parent = self.account.inbox / parent_folder / folder_name
+        self.logger.info(
+            "Create folder '%s'", folder_name,
+        )
+        new_folder = Folder(parent=parent, name=folder_name)
+        new_folder.save()
+
+    def delete_folder(self, folder_name: str = None, parent_folder: str = None) -> bool:
+        """Delete email folder
+
+        :param folder_name: current folder name
+        :param parent_folder: name for the parent folder, by default INBOX
+        :return: True if operation was successful, False if not
+        """
+        if folder_name is None:
+            raise KeyError("'folder_name' is required for delete folder")
+        if parent_folder is None or parent_folder == self.account.inbox:
+            folder_to_delete = self.account.inbox / folder_name
+        else:
+            folder_to_delete = self.account.inbox / parent_folder / folder_name
+        self.logger.info(
+            "Delete folder  '%s'", folder_name,
+        )
+        folder_to_delete.delete()
+
+    def rename_folder(
+        self, oldname: str = None, newname: str = None, parent_folder: str = None
+    ) -> bool:
+        """Rename email folder
+
+        :param oldname: current folder name
+        :param newname: new name for the folder
+        :param parent_folder: name for the parent folder, by default INBOX
+        :return: True if operation was successful, False if not
+        """
+        if oldname is None or newname is None:
+            raise KeyError("'oldname' and 'newname' are required for rename folder")
+        if parent_folder is None or parent_folder == self.account.inbox:
+            parent = self.account.inbox
+        else:
+            parent = self.account.inbox / parent_folder
+        self.logger.info(
+            "Rename folder '%s' to '%s'", oldname, newname,
+        )
+        self.account.inbox.refresh()
+        items = self._get_all_items_in_folder(oldname, parent_folder)
+        old_folder = Folder(parent=parent, name=oldname)
+        old_folder.name = newname
+        old_folder.save()
+        items.move(to_folder=parent / newname)
+        self.delete_folder(oldname, parent_folder)
+
+    def empty_folder(
+        self,
+        folder_name: str = None,
+        parent_folder: str = None,
+        delete_sub_folders: bool = False,
+    ) -> bool:
+        """Empty email folder of all items
+
+        :param folder_name: current folder name
+        :param parent_folder: name for the parent folder, by default INBOX
+        :param delete_sub_folders: delete sub folders or not, by default False
+        :return: True if operation was successful, False if not
+        """
+        if folder_name is None:
+            raise KeyError("'folder_name' is required for empty folder")
+        if parent_folder is None or parent_folder == self.account.inbox:
+            empty_folder = self.account.inbox / folder_name
+        else:
+            empty_folder = self.account.inbox / parent_folder / folder_name
+        self.logger.info("Empty folder '%s'", folder_name)
+        empty_folder.empty(delete_sub_folders=delete_sub_folders)
