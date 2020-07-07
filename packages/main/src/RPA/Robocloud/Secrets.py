514ccfab1a2d37f31ebee641f61cc99114c9fc98
Ossi Rajuvaara <ossi@robocorp.com>
2020-07-07 17:44:07 +0300

    secrets: add support for e2e encryption

--- packages/main/src/RPA/Robocloud/Secrets.py
+++ packages/main/src/RPA/Robocloud/Secrets.py
@@ -1,139 +1,307 @@
-import importlib
+import base64
+import collections
+import copy
 import json
-import os
+import logging
+import traceback
 from abc import abstractmethod, ABCMeta
 
 import requests
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
+
+from RPA.core.helpers import import_by_name, required_env
+
+
+def url_join(*parts):
+    return "/".join(part.strip("/") for part in parts)
 
 
 class RobocloudVaultError(Exception):
-    """Raised when there is problem with Robocloud Vault secrets"""
+    """Raised when there's problem with reading from Robocloud Vault."""
+
+
+class Secret(collections.Mapping):
+    """Container for a secret with name, description, and
+    multiple key-value pairs. Immutable and avoids logging
+    internal values when possible.
+
+    :param name:        Name of secret
+    :param description: Human-friendly description for secret
+    :param values:      Dictionary of key-value pairs stored in secret
+    """
+
+    def __init__(self, name, description, values):
+        self._name = name
+        self._desc = description
+        self._dict = collections.OrderedDict(**values)
+        self._hash = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def description(self):
+        return self._desc
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __contains__(self, key):
+        return key in self._dict
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __repr__(self):
+        return "Secret(name={name}, keys=[{keys}])".format(
+            name=self.name, keys=", ".join(str(key) for key in self.keys())
+        )
+
+
+class BaseSecretManager(metaclass=ABCMeta):
+    """Abstract class for secrets management. Should be used as a
+    base-class for any adapter implementation.
+    """
+
+    @abstractmethod
+    def get_secret(self, secret_name):
+        """Return ``Secret`` object with given name."""
+
+
+class FileSecrets(BaseSecretManager):
+    """Adapter for secrets stored in a JSON file. Supports only
+    plaintext secrets, and should be used mainly for debugging.
+
+    The path to the secrets file can be set with the
+    environment variable ``RPA_SECRET_FILE``, or as
+    an argument to the library.
+
+    The format of the secrets file should be the following:
+
+    ``{
+      name1: {
+        key1: value1,
+        key2: value2
+      },
+      ...
+    }``
+    """
+
+    def __init__(self, secret_file="secrets.json"):
+        self.logger = logging.getLogger(__name__)
+        self.path = required_env("RPA_SECRET_FILE", secret_file)
+        self._data = self.load(self.path)
+
+    def load(self, path):
+        """Load secrets file."""
+        try:
+            with open(path) as fd:
+                data = json.load(fd)
+
+            if not isinstance(data, dict):
+                raise ValueError("Invalid content format")
+
+            return data
+        except (IOError, ValueError) as err:
+            self.logger.error("Failed to load secrets file: %s", err)
+            return {}
+
+    def get_secret(self, secret_name):
+        """Get secret defined with given name from file.
+
+        :param secret_name: Name of secret to fetch
+        :returns:           Secret object
+        :raises KeyError:   No secret with given name
+        """
+        values = self._data.get(secret_name)
+        if values is None:
+            raise KeyError(f"Undefined secret: {secret_name}")
+
+        return Secret(secret_name, "", values)
+
+
+class RobocloudVault(BaseSecretManager):
+    """Adapter for secrets stored in Robocloud Vault.
+
+    The following environment variables should exist:
+
+    - RC_API_SECRET_HOST:   URL to Robocloud Vault API
+    - RC_API_SECRET_TOKEN:  API token with access to Robocloud Vault API
+    - RC_WORKSPACE_ID:      Robocloud Workspace ID
+    """
+
+    ENCRYPTION_SCHEME = "robocloud-vault-transit-v2"
+
+    def __init__(self, *args, **kwargs):
+        # pylint: disable=unused-argument
+        self.logger = logging.getLogger(__name__)
+        # Environment variables set by runner
+        self._host = required_env("RC_API_SECRET_HOST")
+        self._token = required_env("RC_API_SECRET_TOKEN")
+        self._workspace = required_env("RC_WORKSPACE_ID")
+        # Generated lazily on request
+        self.__private_key = None
+        self.__public_bytes = None
+
+    @property
+    def headers(self):
+        """Default request headers."""
+        return {"Authorization": f"Bearer {self._token}"}
+
+    @property
+    def params(self):
+        """Default request parameters."""
+        return {
+            "encryptionSchema": self.ENCRYPTION_SCHEME,
+            "publicKey": self._public_bytes,
+        }
+
+    @property
+    def _private_key(self):
+        """Cryptography private key object."""
+        if self.__private_key is None:
+            self.__private_key = rsa.generate_private_key(
+                public_exponent=65537, key_size=4096, backend=default_backend()
+            )
+
+        return self.__private_key
+
+    @property
+    def _public_bytes(self):
+        """Serialized public key bytes."""
+        if self.__public_bytes is None:
+            self.__public_bytes = base64.b64encode(
+                self._private_key.public_key().public_bytes(
+                    encoding=serialization.Encoding.DER,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                )
+            )
+
+        return self.__public_bytes
+
+    def create_url(self, name):
+        """Create a URL for a specific secret."""
+        return url_join(
+            self._host, "secrets-v1", "workspaces", self._workspace, "secrets", name
+        )
+
+    def get_secret(self, secret_name):
+        """Get secret defined with given name from Robocloud Vault.
+
+        :param secret_name:             Name of secret to fetch
+        :returns:                       Secret object
+        :raises RobocloudVaultError:    Error with API request or response payload
+        """
+        url = self.create_url(secret_name)
+
+        try:
+            response = requests.get(url, headers=self.headers, params=self.params)
+            response.raise_for_status()
+
+            payload = response.json()
+            payload = self._decrypt_payload(payload)
+        except Exception as exc:
+            self.logger.debug(traceback.format_exc())
+            raise RobocloudVaultError(exc)
+
+        return Secret(payload["name"], payload["description"], payload["values"])
+
+    def _decrypt_payload(self, payload):
+        payload = copy.deepcopy(payload)
+
+        fields = payload.pop("encryption", None)
+        if fields is None:
+            raise KeyError("Missing encryption fields from response")
+
+        scheme = fields["encryptionSchema"]
+        if scheme != self.ENCRYPTION_SCHEME:
+            raise ValueError(f"Unexpected encryption scheme: {scheme}")
+
+        aes_enc = base64.b64decode(fields["encryptedAES"])
+        aes_tag = base64.b64decode(fields["authTag"])
+        aes_iv = fields["iv"].encode("utf-8")  # NOTE: This might change
+
+        # Decrypt AES key using our private key
+        aes_key = self._private_key.decrypt(
+            aes_enc,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+
+        # Decrypt actual value using decrypted AES key
+        ciphertext = base64.b64decode(payload.pop("value")) + aes_tag
+        data = AESGCM(aes_key).decrypt(aes_iv, ciphertext, b"")
+        payload["values"] = json.loads(data)
+
+        return payload
 
 
 class Secrets:
-    """Handler for SecretManager. Decides which SecretManager to use based on
-    environment variable `RPA_SECRET_MANAGER`. If environment variable is not
-    defined then `RobocloudSecrets` is used as default SecretManager.
+    """A library for reading secrets, e.g. passwords or tokens, that can
+    be used during a task execution.
 
-    :raises Exception: if configured SecretManager does not implement
-                       `BaseSecretManager` class
-    :raises Exception: if configured SecretManager does not exist in the namespace
+    Primarily used with Robocloud Vault, but supports custom adapters
+    for storing secrets in other databases or local files.
+
+    The selected adapter can be set with the environment
+    variable ``RPA_SECRET_MANAGER``, or the keyword argument ``default_adapter``.
+    Defaults to Robocloud Vault if not defined.
+
+    All other library arguments are passed to the adapter.
+
+    :param default_adapter: Override default secret adapter
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger(__name__)
+
+        default = kwargs.pop("default_adapter", RobocloudVault)
+        adapter = required_env("RPA_SECRET_MANAGER", default)
+
+        self._adapter_factory = self._create_factory(adapter, args, kwargs)
+        self._adapter = None
+
         try:
             BuiltIn().import_library("RPA.RobotLogListener")
         except RobotNotRunningError:
             pass
 
-        self.secretmanager = os.getenv(
-            "RPA_SECRET_MANAGER", "RPA.Robocloud.Secrets.RobocloudVault"
-        )
-        package, modulename = self.secretmanager.rsplit(".", 1)
-        module = importlib.import_module(package)
-        my_class = getattr(module, modulename)
-        if args:
-            self.secretmanager_obj = my_class(args)
-        else:
-            self.secretmanager_obj = my_class()
+    @property
+    def adapter(self):
+        if self._adapter is None:
+            self._adapter = self._adapter_factory()
 
-    def __getattr__(self, name):
-        if self.secretmanager_obj is not None:
-            return getattr(self.secretmanager_obj, name)
-        raise AttributeError("No valid backend")
+        return self._adapter
 
-    def get_keyword_names(self):
-        return ["get_secret"]
+    def _create_factory(self, adapter, args, kwargs):
+        if isinstance(adapter, str):
+            adapter = import_by_name(adapter, __name__)
 
-
-class BaseSecretManager(metaclass=ABCMeta):
-    """Abstract class for Secret management which should be used as a
-    base class for any Secrets implementation class.
-
-    """
-
-    @abstractmethod
-    def get_secret(self, secret_name):
-        pass
-
-
-class FileSecrets(BaseSecretManager):
-    """Handler for secrets stored into a file.
-
-    Following environment variables should exist:
-
-        - RPA_SECRET_FILE : filepath to JSON secrets file
-    """
-
-    def __init__(self, secret_file="secrets.json"):
-        super().__init__()
-        if isinstance(secret_file, tuple):
-            secret_file = " ".join(secret_file)
-        self.secret_file = os.getenv("RPA_SECRET_FILE", secret_file)
-        self.secrets = {}
-        # make checks for the path
-        with open(self.secret_file) as f:
-            self.secrets = json.load(f)
-
-    def get_secret(self, secret_name):
-        """Get secret defined with key `secret_name` and return
-        value of the key.
-
-        If key does not exist raises `KeyError`.
-
-        :param secret_name: secret to fetch
-        :return: value of the `secret_name` key or `None`
-        """
-        secrets = self.secrets[secret_name] if secret_name in self.secrets else None
-        if secrets is None:
-            raise KeyError(f"Undefined secret: {secret_name}")
-        return secrets
-
-
-class RobocloudVault(BaseSecretManager):
-    """Handler for secrets stored into Robocloud SecretsManager.
-
-    Following environment variables should exist:
-
-        - RC_API_SECRET_HOST : URL to Robocloud Secrets API
-        - RC_API_SECRET_TOKEN : API Token for Robocloud Secrets API
-        - RC_WORKSPACE_ID : Robocloud Workspace ID
-
-    """
-
-    def __init__(self, *_):
-        self.secret_host = os.getenv("RC_API_SECRET_HOST", None)
-        self.secret_token = os.getenv("RC_API_SECRET_TOKEN", None)
-        self.workspace_id = os.getenv("RC_WORKSPACE_ID", None)
-        self.headers = {"Authorization": f"Bearer {self.secret_token}"}
-
-    def _get_secret_from_robocloud(self, secret_name):
-        try:
-            request_url = (
-                f"{self.secret_host}/secrets-v1/workspaces/"
-                f"{self.workspace_id}/secrets/{secret_name}"
-            )
-            response = requests.get(request_url, headers=self.headers)
-            # If the response was successful, no Exception will be raised
-            response.raise_for_status()
-            return response.json().get("value", None)
-        except Exception:
-            raise RobocloudVaultError(
-                f"Unable to get secret {secret_name} from RobocloudVault."
+        if not issubclass(adapter, BaseSecretManager):
+            raise ValueError(
+                f"Adapter '{adapter}' does not inherit from BaseSecretManager"
             )
 
+        def factory():
+            return adapter(*args, **kwargs)
+
+        return factory
+
     def get_secret(self, secret_name):
-        """Get secret defined with key `secret_name` and return
-        value of the key.
+        """Read a secret from the configured source, e.g. Robocloud Vault,
+        and return it as a ``Secret`` object.
 
-        If key does not exist raises `KeyError`.
-
-        :param secret_name: secret to fetch
-        :return: value of the `secret_name` key or `None`
+        :param secret_name: Name of secret
         """
-        secrets = self._get_secret_from_robocloud(secret_name)
-        response_as_json = json.loads(secrets) if secrets else None
-        if response_as_json is None:
-            raise KeyError(f"Undefined secret: {secret_name}")
-        return response_as_json
+        return self.adapter.get_secret(secret_name)
--- packages/main/tests/python/test_secrets.py
+++ packages/main/tests/python/test_secrets.py
@@ -1,73 +1,281 @@
+import base64
+import binascii
+import copy
+import json
+import os
+from pathlib import Path
+
+import mock
 import pytest
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.serialization import load_der_public_key
+
+from RPA.Robocloud.Secrets import (
+    Secrets,
+    Secret,
+    RobocloudVault,
+    FileSecrets,
+    BaseSecretManager,
+    RobocloudVaultError,
+)
+
+RESOURCES = Path(__file__).parent / ".." / "resources"
 
 
-class Trollo(object):
-    def __init__(self):
-        pass
+class InvalidBaseClass(object):
+    def get_secret(self, secret_name):
+        assert False, "Should not be called"
 
 
-def test_use_secret_manager_when_environment_is_not_set(monkeypatch):
+class MockAdapter(BaseSecretManager):
+    args = None
+    name = None
+    value = None
+
+    def __init__(self, *args, **kwargs):
+        MockAdapter.args = (args, kwargs)
+
+    def get_secret(self, secret_name):
+        MockAdapter.name = secret_name
+        return MockAdapter.value
+
+
+@pytest.fixture
+def mock_env_default(monkeypatch):
     monkeypatch.delenv("RPA_SECRET_MANAGER", raising=False)
-    from RPA.Robocloud.Secrets import Secrets
-
-    sm = Secrets()
-    assert sm.secretmanager == "RPA.Robocloud.Secrets.RobocloudVault"
 
 
-def test_file_secret_manager(monkeypatch):
-    monkeypatch.setenv("RPA_SECRET_MANAGER", "RPA.Robocloud.Secrets.FileSecrets")
-    monkeypatch.setenv("RPA_SECRET_FILE", "tests/resources/secrets.json")
-    from RPA.Robocloud.Secrets import Secrets
-
-    sm = Secrets()
-    assert sm.secretmanager == "RPA.Robocloud.Secrets.FileSecrets"
-    secrets = sm.get_secret("credentials")
-    assert secrets["sap"]["login"] == "robot"
-    assert secrets["sap"]["password"] == "secret"
-    assert secrets["google"]["apikey"] == "1234567890"
+@pytest.fixture
+def mock_env_vault(monkeypatch):
+    monkeypatch.setenv("RC_API_SECRET_HOST", "mock-url")
+    monkeypatch.setenv("RC_API_SECRET_TOKEN", "mock-token")
+    monkeypatch.setenv("RC_WORKSPACE_ID", "mock-workspace")
 
 
-def test_not_existing_class_as_secret_manager(monkeypatch):
-    monkeypatch.setenv("RPA_SECRET_MANAGER", "RPA.NotExistingSecretManager")
-    from RPA.Robocloud.Secrets import Secrets
+def test_secrets_vault_as_default(mock_env_default, mock_env_vault):
+    library = Secrets()
+    assert isinstance(library.adapter, RobocloudVault)
 
-    with pytest.raises(Exception):
+
+def test_secrets_vault_missing_token(mock_env_default, mock_env_vault, monkeypatch):
+    monkeypatch.delenv("RC_API_SECRET_TOKEN", raising=False)
+    library = Secrets()
+    with pytest.raises(KeyError):
+        _ = library.adapter
+
+
+def test_secrets_custom_adapter_arguments(mock_env_default):
+    library = Secrets("pos-value", key="key-value", default_adapter=MockAdapter)
+    library.get_secret("not-relevant")  # Adapter created on first request
+    assert MockAdapter.args == (("pos-value",), {"key": "key-value"})
+
+
+def test_secrets_custom_adapter_get_secret(mock_env_default):
+    MockAdapter.value = "mock-secret"
+    library = Secrets(default_adapter=MockAdapter)
+    assert library.get_secret("mock-name") == "mock-secret"
+    assert MockAdapter.name == "mock-name"
+
+
+def test_secrets_adapter_missing_import(monkeypatch):
+    monkeypatch.setenv("RPA_SECRET_MANAGER", "RPA.AdapterNotExist")
+    with pytest.raises(ValueError):
         Secrets()
 
 
-def test_invalid_base_class_as_secret_manager(monkeypatch):
-    monkeypatch.setenv("RPA_SECRET_MANAGER", "RPA.Trollo")
-    from RPA.Robocloud.Secrets import Secrets
-
-    with pytest.raises(Exception):
-        Secrets()
+def test_secrets_adapter_invalid_baseclass(mock_env_default):
+    with pytest.raises(ValueError):
+        Secrets(default_adapter=InvalidBaseClass)
 
 
-def test_direct_instance_of_robocloud_vault():
-    from RPA.Robocloud.Secrets import RobocloudVault
+def test_secret_properties():
+    secret = Secret(name="name-value", description="description-value", values={},)
 
-    sm = RobocloudVault()
-    assert sm.headers is not None
+    assert secret.name == "name-value"
+    assert secret.description == "description-value"
 
 
-def test_error_with_robocloud_vault_when_no_configuration_exists():
-    from RPA.Robocloud.Secrets import RobocloudVault, RobocloudVaultError
+def test_secret_get():
+    secret = Secret(
+        name="name-value",
+        description="description-value",
+        values={"key_one": "value_one", "key_two": "value_two"},
+    )
 
-    sm = RobocloudVault()
+    assert secret["key_one"] == "value_one"
+    assert secret["key_two"] == "value_two"
+    with pytest.raises(KeyError):
+        _ = secret["key_invalid"]
+
+
+def test_secret_iterate():
+    secret = Secret(
+        name="name-value",
+        description="description-value",
+        values={"key_one": "value_one", "key_two": "value_two"},
+    )
+
+    assert list(secret) == ["key_one", "key_two"]
+
+
+def test_secret_contains():
+    secret = Secret(
+        name="name-value",
+        description="description-value",
+        values={"key_one": "value_one", "key_two": "value_two"},
+    )
+
+    assert "key_two" in secret
+
+
+def test_secret_print():
+    secret = Secret(
+        name="name-value",
+        description="description-value",
+        values={"key_one": "value_one", "key_two": "value_two"},
+    )
+
+    repr_string = repr(secret)
+    assert "value_one" not in repr_string
+    assert "value_two" not in repr_string
+
+    str_string = str(secret)
+    assert "value_one" not in str_string
+    assert "value_two" not in str_string
+
+
+def test_secret_immutable():
+    secret = Secret(name="name", description="desc", values={"key": "value"},)
+
+    with pytest.raises(AttributeError):
+        secret.name = "test"
+
+    with pytest.raises(AttributeError):
+        secret.description = "test"
+
+    with pytest.raises(TypeError):
+        secret["key"] = "test"
+
+
+def test_adapter_filesecrets_from_arg(monkeypatch):
+    monkeypatch.delenv("RPA_SECRET_FILE", raising=False)
+
+    adapter = FileSecrets(RESOURCES / "secrets.json")
+    secret = adapter.get_secret("windows")
+    assert isinstance(secret, Secret)
+    assert "password" in secret
+    assert secret["password"] == "secret"
+
+
+def test_adapter_filesecrets_from_env(monkeypatch):
+    monkeypatch.setenv("RPA_SECRET_FILE", str(RESOURCES / "secrets.json"))
+
+    adapter = FileSecrets()
+    secret = adapter.get_secret("windows")
+    assert isinstance(secret, Secret)
+    assert "password" in secret
+    assert secret["password"] == "secret"
+
+
+def test_adapter_filesecrets_invalid_file(monkeypatch):
+    monkeypatch.setenv("RPA_SECRET_FILE", str(RESOURCES / "not-a-file.json"))
+
+    # Should not raise
+    _ = FileSecrets()
+
+
+def test_adapter_filesecrets_unknown_secret(monkeypatch):
+    monkeypatch.setenv("RPA_SECRET_FILE", str(RESOURCES / "secrets.json"))
+
+    adapter = FileSecrets()
+    with pytest.raises(KeyError):
+        secret = adapter.get_secret("not-exist")
+
+
+@mock.patch("RPA.Robocloud.Secrets.requests")
+def test_adapter_vault_request(mock_requests, mock_env_default, mock_env_vault):
+    mock_requests.get.return_value.json.return_value = {
+        "name": "mock-name",
+        "description": "mock-desc",
+        "value": {"mock-key": "mock-value"},
+        "encryption": {},
+    }
+
+    def mock_decrypt(payload):
+        # Decrypt tested separately
+        payload = copy.deepcopy(payload)
+        payload["values"] = payload.pop("value")
+        return payload
+
+    adapter = RobocloudVault()
+    adapter._decrypt_payload = mock_decrypt
+
+    secret = adapter.get_secret("mock-name")
+    assert secret.name == "mock-name"
+    assert secret.description == "mock-desc"
+    assert secret["mock-key"] == "mock-value"
+
+    mock_requests.get.assert_called_once_with(
+        "mock-url/secrets-v1/workspaces/mock-workspace/secrets/mock-name",
+        headers={"Authorization": "Bearer mock-token"},
+        params={
+            "encryptionSchema": "robocloud-vault-transit-v2",
+            "publicKey": adapter._public_bytes,
+        },
+    )
+
+
+@mock.patch("RPA.Robocloud.Secrets.requests")
+def test_adapter_vault_error(mock_requests, mock_env_vault):
+    mock_requests.get.side_effect = RuntimeError("Some request error")
+
+    adapter = RobocloudVault()
     with pytest.raises(RobocloudVaultError):
-        sm.get_secret("myspecialsecret")
+        adapter.get_secret("mock-name")
 
 
-def test_direct_instance_of_file_secret_manager(monkeypatch):
-    monkeypatch.setenv("RPA_SECRET_FILE", "tests/resources/secrets.json")
-    from RPA.Robocloud.Secrets import FileSecrets
+def test_adapter_vault_encryption(mock_env_vault):
+    data = json.dumps({"mock-key": "mock-value"}).encode("utf-8")
 
-    sm = FileSecrets()
-    secrets_windows = sm.get_secret("windows")
-    secrets_robocloud = sm.get_secret("robocloud")
-    assert secrets_windows["domain"] == "windows"
-    assert secrets_windows["login"] == "robot"
-    assert secrets_windows["password"] == "secret"
-    assert secrets_robocloud["url"] == "http://robocloud.ai/"
-    assert secrets_robocloud["login"] == "robot"
-    assert secrets_robocloud["password"] == "secret"
+    # Cloud encrypts secret with symmetric encryption
+    key = AESGCM.generate_key(bit_length=128)
+    aesgcm = AESGCM(key)
+    nonce = binascii.hexlify(os.urandom(12))
+    ct_data = aesgcm.encrypt(nonce, data, b"")
+
+    # Cloud uses client-supplied public key to encrypt symmetric key
+    adapter = RobocloudVault()
+    public_key = load_der_public_key(
+        base64.b64decode(adapter._public_bytes), default_backend()
+    )
+
+    ct_key = public_key.encrypt(
+        key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
+
+    ct_data, auth_tag = ct_data[:-16], ct_data[-16:]
+
+    # Binary fields are base64 encoded, except nonce/iv (will possibly change)
+    payload = {
+        "name": "mock-name",
+        "description": "mock-desc",
+        "value": base64.b64encode(ct_data).decode("utf-8"),
+        "encryption": {
+            "encryptionSchema": adapter.ENCRYPTION_SCHEME,
+            "encryptedAES": base64.b64encode(ct_key).decode("utf-8"),
+            "authTag": base64.b64encode(auth_tag).decode("utf-8"),
+            "iv": nonce.decode("utf-8"),
+        },
+    }
+
+    response = adapter._decrypt_payload(payload)
+    assert response["name"] == "mock-name"
+    assert response["values"]["mock-key"] == "mock-value"
