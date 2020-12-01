a4ab278d2548e4e611a8341ee1d241b157b796cb
Kerkko Pelttari <kerkko@robocorp.com>
2020-12-01 17:50:45 +0200

    Iterate on api_changes.py

--- tools/api_changes.py
+++ tools/api_changes.py
@@ -1,29 +1,30 @@
-
-from io import UnsupportedOperation
 import subprocess
 from functools import partial
-
+import re
 
 def main():
     run_and_capture = partial(subprocess.run, shell=True, check=True, capture_output=True, text=True)
-    result = run_and_capture("git status --porcelain docs/source/json/")
-    change_lines = result.stdout.split("\n")
-    for row in change_lines:
-        if row:
-            status, filename = row.split()
-            if status == "D":
-                print(f"Please document removal of {filename}")
-            elif status == "M":
-                print(f"Please document modification of {filename}")
-                result = run_and_capture(f"git diff --unified=0 {filename}")
-                without_header = result.stdout.split("\n")[5:]
-                print(without_header)
-            elif status == "A":
-                print(f"Please document addition of {filename}")
-                pass
-            else:
-                raise UnsupportedOperation(f"{filename} has invalid git status")
 
+    filename = "docs/build/html/latest.json"
+    result = run_and_capture(f"git diff --unified=10 {filename}")
+    regex_for_timestamp_changes = r'([-+] {8}\"generated\": \"[\d\-: ]{19}\",\n){2}'
+    regex_for_linecounts = r'@@.*@@\n'
+    # print(f"result.stdout: {result.stdout}")
+    header_omitted = result.stdout[4:]
+    diff_chunks = re.split(regex_for_linecounts, header_omitted)
+
+    replaced = 0
+    print(f"Please document following changes:")
+    for chunk in diff_chunks:
+        if 'object object at' in chunk or re.search(regex_for_timestamp_changes, chunk):
+            replaced += 1
+            continue
+        if chunk:
+            print(chunk)
+            # without_header = result.stdout.split("\n")[5:]
+    
+    print(f"Omitted {replaced} irrelevant changes")
+    print("After documenting the changes, git commit docs/build/html/latest.json")
 
 if __name__ == "__main__":
     main()
