22e450fd493d140fecacce71f649c8cfd631f0d3
Ossi Rajuvaara <ossi@robocorp.com>
2021-01-11 14:38:45 +0200

    docs: dynamic banner height

--- docs/source/_static/robocorp.js
+++ docs/source/_static/robocorp.js
@@ -1,4 +1,6 @@
 (function (window) {
+  var showBanner = false;
+
   function setCookie(name, value, days) {
     var expires = "";
     if (days) {
@@ -27,24 +29,35 @@
   function bannerShow() {
     if (!getCookie("bannerDismiss")) {
       var banner = document.getElementById("robocorpBanner");
-      var sidebar = document.getElementsByClassName("wy-side-scroll")[0];
-      var content = document.getElementsByClassName("wy-nav-content-wrap")[0];
       banner.style.display = "block";
-      sidebar.style.paddingTop = "50px";
-      content.style.paddingTop = "50px";
+      showBanner = true;
+      resizePadding();
     }
   }
 
   function bannerDismiss() {
     setCookie("bannerDismiss", "1", 7);
     var banner = document.getElementById("robocorpBanner");
+    banner.style.display = "none";
+    showBanner = false;
+    resizePadding();
+  }
+
+  function resizePadding() {
+    if (showBanner) {
+      var banner = document.getElementById("robocorpBanner");
+      var padding = banner.offsetHeight + "px";
+    } else {
+      var padding = "0px";
+    }
+
     var sidebar = document.getElementsByClassName("wy-side-scroll")[0];
     var content = document.getElementsByClassName("wy-nav-content-wrap")[0];
-    banner.style.display = "none";
-    sidebar.style.paddingTop = "0px";
-    content.style.paddingTop = "0px";
+    sidebar.style.paddingTop = padding;
+    content.style.paddingTop = padding;
   }
 
   window.bannerDismiss = bannerDismiss;
   window.addEventListener("load", bannerShow);
-})(window)
+  window.addEventListener("resize", resizePadding);
+})(window);
