97a4b9c82476dc0eb5a476900a928a42074c8556
Henrik Vesterinen <henrik@robocorp.com>
2021-01-18 11:17:16 +0200

    main.tests.browser: improve tests

--- packages/main/tests/python/test_browser.py
+++ packages/main/tests/python/test_browser.py
@@ -5,23 +5,17 @@ from unittest import TestCase
 
 import pytest
 
+from RPA.Browser.Selenium import Selenium
 from RPA.PDF import PDF
+
 from . import RESOURCE_DIR, temp_filename
 
 
+selenium_lib = Selenium()
+
+
 class TestBrowserFunctionality(TestCase):
-    def test_imports(self):
-        from RPA.Browser import Browser
-        from RPA.Browser.Selenium import Selenium
-
-        Browser()
-        Selenium()
-
     def test_print_to_pdf(self):
-        from RPA.Browser.Selenium import Selenium
-
-        selenium_lib = Selenium()
-
         testfile = RESOURCE_DIR / "browser_docs.html"
         selenium_lib.open_available_browser(f"file://{testfile}", headless=True)
         with temp_filename() as tmp_file:
@@ -31,10 +25,6 @@ class TestBrowserFunctionality(TestCase):
             assert "Please explicitly use either RPA.Browser.Selenium" in text[1]
 
     def test_print_to_pdf_different_from_start_page(self):
-        from RPA.Browser.Selenium import Selenium
-
-        selenium_lib = Selenium()
-
         startpage = RESOURCE_DIR / "alert.html"
         testfile = RESOURCE_DIR / "browser_docs.html"
         selenium_lib.open_available_browser(f"file://{startpage}", headless=True)
@@ -43,3 +33,16 @@ class TestBrowserFunctionality(TestCase):
             text = PDF().get_text_from_pdf(tmp_file)
 
             assert "Please explicitly use either RPA.Browser.Selenium" in text[1]
+
+    def test_print_to_pdf_exception_on_non_supported_driver(self):
+        testfile = RESOURCE_DIR / "browser_docs.html"
+        selenium_lib.open_available_browser(
+            f"file://{testfile}", browser_selection="firefox", headless=True
+        )
+
+        expected = "PDF printing works only with Chrome/Chromium"
+
+        with pytest.raises(NotImplementedError) as err:
+            selenium_lib.print_to_pdf(output_path=None)
+
+        assert str(err.value) == expected
