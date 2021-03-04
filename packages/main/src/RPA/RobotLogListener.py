963795163e23c266cd7ea5c15ef7cfcf78273509
Ossi Rajuvaara <ossi@robocorp.com>
2021-03-04 16:10:08 +0200

    listener: ignore unused-import warnings

--- packages/main/src/RPA/RobotLogListener.py
+++ packages/main/src/RPA/RobotLogListener.py
@@ -3,4 +3,6 @@ Dummy module importing core library `RobotLogListener`,
 because imports to core package should be done only by
 the library itself.
 """
+# flake8: noqa
+# pylint: disable=unused-import
 from RPA.core.RobotLogListener import RobotLogListener
