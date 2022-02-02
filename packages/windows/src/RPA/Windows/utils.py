9d6cc59d239576be1b31a86924e0e3c5fd993617
Cosmin Poieana <cosmin@robocorp.com>
2022-02-02 14:57:25 +0000

    Fix COMError importing in non-windows

--- packages/windows/src/RPA/Windows/utils.py
+++ packages/windows/src/RPA/Windows/utils.py
@@ -2,7 +2,6 @@ import platform
 from typing import Dict, Optional
 
 import psutil
-from comtypes import COMError
 
 
 def get_process_list() -> Dict:
@@ -38,6 +37,11 @@ def call_attribute_if_available(object_name, attribute_name):
 def window_or_none(
     window, timeout: float = 5
 ) -> Optional["WindowsElement"]:  # noqa: F821
+    if is_windows():
+        from comtypes import COMError
+    else:
+        COMError = Exception
+
     if window and window.item:
         if hasattr(window.item, "Exists"):
             return window if window.item.Exists(maxSearchSeconds=timeout) else None
