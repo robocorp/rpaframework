62bce9ba24e4af1c26b2d5d0da0b826ebfe663ee
Cosmin Poieana <cosmin@robocorp.com>
2022-03-23 10:56:18 +0200

    Base `windows` on latest `core` (#435)
    * Base `windows` on latest `core` (#432)
    
    * Base windows on core
    
    * Referencing all sub-packages
    
    * Align core with latest windows 2.3.2
    
    * Minimum required access when listing windows
    
    * Vendor DynamicCore and proper register libs
    
    * Align recording functionality
    
    * Fix linting
    
    * Fix docstrings and pylint ignores
    
    * Poetry update
    
    * Fix RecordElement types
    
    * Recording list type annotation
    
    * Fix importing on non-Windows machines
    
    * Fix inspect recording params order
    
    * Use mixin for the library creation
    
    * Fix recording inspect
    
    * Bugfix in recording
    
    * Fix linting
    
    * Make main include windows on Windows platforms
    
    * Revert windows inclusion into main for now
    
    * Fix Calculator test on Windows 11

--- packages/windows/src/RPA/Windows/keywords/context.py
+++ packages/windows/src/RPA/Windows/keywords/context.py
@@ -1,20 +1,21 @@
-import contextlib
-import functools
-from typing import Optional
+# pylint: disable=unused-import
+from RPA.core.windows.context import (  # noqa: F401
+    ElementNotFound,
+    WindowControlError,
+    WindowsContext as LibraryContext,
+    with_timeout,
+)
 
-from RPA.Windows.utils import IS_WINDOWS
 
-if IS_WINDOWS:
-    import uiautomation as auto
-    from comtypes import COMError  # noqa
+class ActionNotPossible(ValueError):
+    """Action is not possible for the given Control."""
+
+
+# Not yet used exceptions:
 
 
 class ControlNotFound(ValueError):
     """No matching controls were found."""
-
-
-class ElementNotFound(ValueError):
-    """No matching elements were found."""
 
 
 class MultipleControlsFound(ValueError):
@@ -23,79 +24,3 @@ class MultipleControlsFound(ValueError):
 
 class TimeoutException(ValueError):
     """Timeout reached while waiting for condition."""
-
-
-class WindowControlError(ValueError):
-    """Matching window was not found"""
-
-
-class ActionNotPossible(ValueError):
-    """Action is not possible for the given Control"""
-
-
-class LibraryContext:
-    """Shared context for all keyword libraries."""
-
-    def __init__(self, ctx):
-        self.ctx = ctx
-
-    @property
-    def logger(self):
-        return self.ctx.logger
-
-    @property
-    def current_timeout(self) -> float:
-        # This value can change based on `auto.SetGlobalSearchTimeout(...)` calls.
-        return auto.uiautomation.TIME_OUT_SECOND
-
-    def _window_or_none(
-        self, window: "WindowsElement"  # noqa: F821
-    ) -> Optional["WindowsElement"]:  # noqa: F821
-        if window and window.item:
-            try:
-                window.item.BoundingRectangle
-            except COMError:  # pylint: disable=broad-except
-                # Failure to get the bounding rectangle proves that the window doesn't
-                #  exist anymore.
-                return None
-
-            return window
-
-        return None
-
-    @property
-    def anchor(self) -> Optional["WindowsElement"]:  # noqa: F821
-        return self._window_or_none(self.ctx.anchor_element)
-
-    @property
-    def window(self) -> Optional["WindowsElement"]:  # noqa: F821
-        return self._window_or_none(self.ctx.window_element)
-
-    @contextlib.contextmanager
-    def set_timeout(self, timeout: Optional[float] = None):
-        """Context manager that sets a custom temporary `timeout`."""
-        try:
-            if timeout is not None:
-                auto.SetGlobalSearchTimeout(timeout)
-                self.logger.info("Locator timeout set to: %f", self.current_timeout)
-            yield
-        finally:
-            if timeout is not None:
-                auto.SetGlobalSearchTimeout(self.ctx.global_timeout)
-                self.logger.debug(
-                    "Locator timeout set back to global value: %f", self.current_timeout
-                )
-
-
-def with_timeout(func):
-    """Applies a temporary `timeout` over the entire decorated context method."""
-
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
-        timeout: Optional[float] = kwargs.pop("timeout", None)
-        with self.set_timeout(timeout):
-            # Do not send on purpose the `timeout` back to the original decorated
-            #  function. (as the temporary timeout setting is currently active)
-            return func(self, *args, **kwargs)
-
-    return wrapper
