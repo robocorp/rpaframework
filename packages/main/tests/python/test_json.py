a26a7568d5750ca649b026fb6841f5edc4d4bd82
Cosmin Poieana <cosmin@robocorp.com>
2022-05-24 14:00:41 +0200

    Fix JSON deletion with filtering (#509)
    * Fix JSON deletion with filtering
    
    * Opportunistic refactoring

--- packages/main/tests/python/test_json.py
+++ packages/main/tests/python/test_json.py
@@ -23,16 +23,39 @@ ORDERS = """
     }
   ]
 }
-""".strip()
+"""
+
+MARKS = [
+    '{"People": [{"Name": "Mark", "Email": "mark@robocorp.com"}, {"Name": "Jane", '
+    '"Extra": 1}]}',
+    '{"People": {"a": 1, "b": {"Name": "Mark", "Email": "mark@robocorp.com"}, "c": '
+    '{"Name": "Jane", "Extra": 1}}}',
+    '{"People": {"a": 1, "b": {"z": {"Name": "Mark", "Email": "mark@robocorp.com"}}, '
+    '"c": {"Name": "Jane", "Extra": 1}}}',
+]
 
 
-@pytest.fixture()
-def orders():
-    return JSON().convert_string_to_json(ORDERS)
+@pytest.fixture
+def lib():
+    return JSON()
 
 
-def test_json_expression(orders):
-    result = JSON().get_values_from_json(orders, "$..address")
+def str2json(json_string, *, lib):
+    return lib.convert_string_to_json(json_string.strip())
+
+
+@pytest.fixture
+def orders(lib):
+    return str2json(ORDERS, lib=lib)
+
+
+@pytest.fixture(params=MARKS)
+def mark(lib, request):
+    return str2json(request.param, lib=lib)
+
+
+def test_json_expression(lib, orders):
+    result = lib.get_values_from_json(orders, "$..address")
     assert result == [
         "Streetroad 123",
         "Streetroad 123",
@@ -41,25 +64,41 @@ def test_json_expression(orders):
     ]
 
 
-def test_json_expression_filter(orders):
-    result = JSON().get_values_from_json(orders, "$..orders[?(@.price>100)]")
+def test_json_expression_filter(lib, orders):
+    result = lib.get_values_from_json(orders, "$..orders[?(@.price>100)]")
     assert result == [
         {"address": "Streetroad 123", "price": 103.2},
         {"address": "Streetroad 123", "price": 2330.01},
     ]
 
 
-def test_json_single_value(orders):
-    assert JSON().get_value_from_json(orders, "$.clients[0].name") == "Johnny Example"
+def test_json_single_value(lib, orders):
+    assert lib.get_value_from_json(orders, "$.clients[0].name") == "Johnny Example"
 
 
-def test_json_single_value_default(orders):
+def test_json_single_value_default(lib, orders):
     assert (
-        JSON().get_value_from_json(orders, "$.clients[3].name", default="Notexist")
+        lib.get_value_from_json(orders, "$.clients[3].name", default="Notexist")
         == "Notexist"
     )
 
 
-def test_json_update(orders):
-    result = JSON().update_value_to_json(orders, "$.clients[*].name", "John Malkovich")
+def test_json_update(lib, orders):
+    result = lib.update_value_to_json(orders, "$.clients[*].name", "John Malkovich")
     assert all(client["name"] == "John Malkovich" for client in result["clients"])
+
+
+@pytest.mark.parametrize(
+    "expr",
+    [
+        '$.People[?(@..Name=="Mark")]',
+        '$.People[?(@..Name=="Mark") & (@..Email=="mark@robocorp.com")]',
+        '$[?(@..Name=="Other")] | $[?(@..Email=="mark@robocorp.com")]',
+        '($[?(@..Name=="Other")] | $[?(@..Email=="mark@robocorp.com")])[?(@..Email=="mark@robocorp.com")]',
+        '($|($.*)|($.*.*))[?(@.Name=="Mark")]',
+    ],
+)
+def test_delete_from_json(lib, mark, expr):
+    mark_dict = lib.delete_from_json(mark, expr)
+    mark_str = lib.convert_json_to_string(mark_dict)
+    assert "Mark" not in mark_str
