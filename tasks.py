3970a1329bd11a867c48372f1dbff3217b9ad057
Cosmin Poieana <cosmin@robocorp.com>
2022-06-14 11:03:14 +0300

    Improve invoke tasks (local packages)

--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
-from glob import glob
 import platform
 import re
 import shutil
-import toml
 import subprocess
+import toml
+from glob import glob
 from pathlib import Path
+
 from invoke import task, call
 
 
@@ -16,14 +17,6 @@ def _git_root():
 
 GIT_ROOT = _git_root()
 PACKAGES_ROOT = GIT_ROOT / "packages"
-
-if platform.system() != "Windows":
-    ACTIVATE_PATH = GIT_ROOT / ".venv" / "bin" / "activate"
-    ACTIVATE = f"source {ACTIVATE_PATH}"
-else:
-    ACTIVATE_PATH = GIT_ROOT / ".venv" / "Scripts" / "activate"
-    ACTIVATE = f"{ACTIVATE_PATH}.bat"
-
 
 CLEAN_PATTERNS = [
     "coverage",
@@ -40,7 +33,7 @@ def _get_package_paths():
     package_paths = {}
     for project_toml in project_tomls:
         project_config = toml.load(project_toml)
-        package_paths[project_config["tool"]["poetry"]["name"]] = Path(
+        package_paths[str(project_config["tool"]["poetry"]["name"])] = Path(
             project_toml
         ).parent
     return package_paths
@@ -80,28 +73,31 @@ def install(ctx, reset=False):
     .venv to the lock file.
     """
     if reset:
-        packages = _get_package_paths()
-        with ctx.prefix(ACTIVATE):
-            result = pip(ctx, "freeze", echo=False, hide="out")
-            local_packages = re.findall(
-                f"^{r'(?=={2})|'.join([str(k) for k in packages.keys()])}(?=={2})",
-                result.stdout,
-                re.MULTILINE,
-            )
-            for local_package in local_packages:
-                pip(ctx, f"uninstall {local_package} -y")
+        our_packages = _get_package_paths()
+        pip_freeze = pip(ctx, "freeze", echo=False, hide="out")
+        # Identifies locally installed packages in development mode.
+        #  (not from PyPI)
+        package_exprs = [rf"{name}(?=={{2}})" for name in our_packages]
+        pattern = "|".join(package_exprs)
+        local_packages = re.findall(
+            pattern,
+            pip_freeze.stdout,
+            re.MULTILINE | re.IGNORECASE,
+        )
+        for local_package in local_packages:
+            pip(ctx, f"uninstall {local_package} -y")
         poetry(ctx, "install --remove-untracked")
     else:
         poetry(ctx, "install")
 
 
-@task(pre=[call(install, reset=True)], iterable=["package"])
-def install_local(ctx, package):
+@task(pre=[call(install, reset=True)], iterable=["packages"])
+def install_local(ctx, packages):
     """Installs local environment with provided package in local
     editable form instead of from PyPi. This task always resets
     the virtual environment first.
 
-    Package must exist as a subfolder module in ``\\packages``,
+    Package must exist as a sub-folder module in ``\\packages``,
     see those packages' ``pyproject.toml`` for package names.
     If ran with no packages, all optional packages will be installed
     from PyPi.
@@ -110,13 +106,13 @@ def install_local(ctx, package):
     that is a cross between all local packages requested. It may
     not be stable.
     """
-    if package:
-        packages = _get_package_paths()
-        for pkg in package:
-            # activate meta context
-            with ctx.prefix(ACTIVATE):
-                # uninstall package
-                pip(ctx, f"uninstall {pkg} -y")
-                # cd to selected package and run poetry install
-                with ctx.cd(packages[pkg]):
-                    poetry(ctx, "install")
+    if not packages:
+        return
+
+    our_packages = _get_package_paths()
+    for package in packages:
+        # Installs our package in development mode under the currently active venv.
+        #  (local package)
+        pip(ctx, f"uninstall {package} -y")
+        with ctx.cd(our_packages[package]):
+            poetry(ctx, "install")
