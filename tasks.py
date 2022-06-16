c1ab7213df0bbef5c4db751a5d8fb43d97c4f2ae
Blake Waud <blake@robocorp.com>
2022-06-16 16:39:45 -0400

    Add `all` flag to clean and improve dependency install and import

--- tasks.py
+++ tasks.py
@@ -3,10 +3,16 @@ import os
 import platform
 import re
 import shutil
-import toml
 import subprocess
 from pathlib import Path
-from invoke import task, call
+from invoke import task, call, ParseError
+
+try:
+    import toml
+except ModuleNotFoundError:
+    DEPENDENCIES_AVAILABLE = False
+else:
+    DEPENDENCIES_AVAILABLE = True
 
 
 def _git_root():
@@ -70,14 +76,22 @@ def pip(ctx, command, **kwargs):
     return _run(ctx, "pip", command, **kwargs)
 
 
+def package_invoke(ctx, directory, command, **kwargs):
+    with ctx.cd(directory):
+        return _run(ctx, "invoke", command, **kwargs)
+
+
 @task()
-def clean(ctx, venv=True, docs=False):
+def clean(ctx, venv=True, docs=False, all=False):
     """Cleans the virtual development environment by
     completely removing build artifacts and the .venv.
     You can set ``--no-venv`` to avoid this default.
 
     If ``--docs`` is supplied, the build artifacts for
     local documentation will also be cleaned.
+
+    You can set flag ``all`` to clean all packages as
+    well.
     """
     union_clean_patterns = []
     if venv:
@@ -92,34 +106,78 @@ def clean(ctx, venv=True, docs=False):
                 os.remove(path)
             except OSError:
                 pass
+    if all:
+        our_packages = _get_package_paths()
+        for package_path in our_packages.values():
+            package_invoke(ctx, package_path, "clean")
 
 
 @task
+def setup_poetry(ctx, username=None, password=None, token=None):
+    """Configure local poetry installation for development.
+    If you provide ``username`` and ``password``, you can
+    also configure your pypi access. Our version of poetry
+    uses ``keyring`` so the password is not stored in the
+    clear.
+
+    Alternatively, you can set ``token`` to use a pypi token, be sure
+    to include the ``pypi-`` prefix in the token.
+    """
+    poetry(ctx, "config -n --local virtualenvs.in-project true")
+    poetry(ctx, "config -n --local virtualenvs.create true")
+    poetry(ctx, "config -n --local virtualenvs.path null")
+    poetry(ctx, "config -n --local experimental.new-installer true")
+    poetry(ctx, "config -n --local installer.parallel true")
+    poetry(
+        ctx,
+        "config -n --local repositories.devpi.url https://devpi.robocorp.cloud/ci/test",
+    )
+    if username and password and token:
+        raise ParseError(
+            "You cannot specify username-password combination and token simultaneously"
+        )
+    if username ^ password:
+        raise ParseError("You must specify both username and password")
+    if username and password:
+        poetry(ctx, f"config -n http-basic.pypi {username} {password}")
+    if token:
+        poetry(ctx, f"config -n pypi-token.pypi {token}")
+
+
+@task(pre=[setup_poetry])
 def install(ctx, reset=False):
     """Install development environment. If ``reset`` is set,
     poetry will remove untracked packages, reverting the
     .venv to the lock file.
+
+    If ``reset`` is attempted before an initial install, it
+    is ignored.
     """
-    if reset:
-        our_packages = _get_package_paths()
-        with ctx.prefix(ACTIVATE):
-            pip_freeze = pip(ctx, "freeze", echo=False, hide="out")
-            # Identifies locally installed packages in development mode.
-            #  (not from PyPI)
-            package_exprs = [
-                rf"{name}(?=={{2}})" for name in our_packages if name != "rpaframework"
-            ]
-            pattern = "|".join(package_exprs)
-            local_packages = re.findall(
-                pattern,
-                pip_freeze.stdout,
-                re.MULTILINE | re.IGNORECASE,
-            )
-            for local_package in local_packages:
-                pip(ctx, f"uninstall {local_package} -y")
-        poetry(ctx, "install --remove-untracked")
-    else:
+    if not DEPENDENCIES_AVAILABLE:
         poetry(ctx, "install")
+    else:
+        if reset:
+            our_packages = _get_package_paths()
+            with ctx.prefix(ACTIVATE):
+                pip_freeze = pip(ctx, "freeze", echo=False, hide="out")
+                # Identifies locally installed packages in development mode.
+                #  (not from PyPI)
+                package_exprs = [
+                    rf"{name}(?=={{2}})"
+                    for name in our_packages
+                    if name != "rpaframework"
+                ]
+                pattern = "|".join(package_exprs)
+                local_packages = re.findall(
+                    pattern,
+                    pip_freeze.stdout,
+                    re.MULTILINE | re.IGNORECASE,
+                )
+                for local_package in local_packages:
+                    pip(ctx, f"uninstall {local_package} -y")
+            poetry(ctx, "install --remove-untracked")
+        else:
+            poetry(ctx, "install")
 
 
 @task(pre=[call(install, reset=True)], iterable=["package"])
