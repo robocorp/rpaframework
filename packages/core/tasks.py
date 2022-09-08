fa6f4f82f732148b3bfdcc9811078ece67dacbf6
Blake Waud <blake@robocorp.com>
2022-09-08 14:10:36 -0400

    Set all package's `tasks.py` file to common collection style

--- packages/core/tasks.py
+++ packages/core/tasks.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from invoke import Collection
 
 REPO_ROOT = Path(__file__).parents[2].resolve()
-PACKAGE_DIR = REPO_ROOT / "packages" / "google"
+PACKAGE_DIR = REPO_ROOT / "packages" / "core"
 
 # Note to VSCode users, in order to eliminate Pylance import errors
 # in the IDE, you must add the following settings to your local project
