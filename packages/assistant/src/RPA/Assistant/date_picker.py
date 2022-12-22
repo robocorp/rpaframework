f4bd1720e29a97ba0dd2cbc7666898c68a8fa593
Bogdan Condurache <bogdan@robocorp.com>
2022-12-22 14:37:29 +0200

    add fixme

--- packages/assistant/src/RPA/Assistant/date_picker.py
+++ packages/assistant/src/RPA/Assistant/date_picker.py
@@ -15,8 +15,9 @@ from flet import (
 from flet.dropdown import Option
 from flet.icons import ARROW_BACK_ROUNDED, ARROW_FORWARD_ROUNDED, CALENDAR_MONTH
 
-
+# FIXME: This doesn't render the components properly so it can't be used right now
 class DatePicker(UserControl):
+
     def _year_increment(self, e):
         self.selected_year += 1
         self.header_element[1].value = self.selected_year
