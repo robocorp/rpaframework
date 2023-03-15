aa880a0e5a56e150b3d8a473c03743a8aff7ccba
Mika Hänninen <mika.hanninen@gmail.com>
2023-03-15 11:35:21 +0200

    Fix `RPA.Calendar` unit & robot tests (#889)
    * calendar. fix python test
    
    * Fix Calendar tests
    
    * Fix Selenium flaky test
    
    * Poetry update
    
    * Fix FileSystem tests
    
    * Fix results workspace cleanup with file tests
    
    ---------
    
    Co-authored-by: Cosmin Poieana <cosmin@robocorp.com>

--- packages/main/tests/python/test_calendar.py
+++ packages/main/tests/python/test_calendar.py
@@ -17,12 +17,16 @@ def test_returning_holidays(library):
 @pytest.mark.freeze_time("2023-03-09")
 def test_set_locale_spanish(library):
     library.set_locale("es")
-    now = library.time_now(return_format="dddd DD MMMM YYYY")
+    now = library.time_now(
+        timezone="Europe/Helsinki", return_format="dddd DD MMMM YYYY"
+    )
     assert now == "jueves 09 marzo 2023"
 
 
 @pytest.mark.freeze_time("2023-03-09")
 def test_set_locale_english(library):
     library.set_locale("en")
-    now = library.time_now(return_format="dddd DD MMMM YYYY")
+    now = library.time_now(
+        timezone="Europe/Helsinki", return_format="dddd DD MMMM YYYY"
+    )
     assert now == "Thursday 09 March 2023"
