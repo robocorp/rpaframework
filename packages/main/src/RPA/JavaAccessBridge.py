f7d8a88ba2b1f4f13e628e940ff923c1ea6ab34d
Mika Hänninen <mika.hanninen@gmail.com>
2023-06-02 12:27:25 +0300

    Fix `RPA.JavaAccessBridge` issue with `Click Element` (#978)
    * update Click Element
    * add wait_until_element_exist keyword

--- packages/main/src/RPA/JavaAccessBridge.py
+++ packages/main/src/RPA/JavaAccessBridge.py
@@ -555,7 +555,7 @@ class JavaAccessBridge:
          if False will use Desktop().press_keys()
         """
         target = self._get_matching_element(locator, index)
-        self._click_element_middle(target, "double click")
+        self.click_element(target, click_type="double click")
 
         if not self.ignore_callbacks:
             target.request_focus()
@@ -786,6 +786,7 @@ class JavaAccessBridge:
         index: int = 0,
         action: bool = True,
         timeout: int = 10,
+        click_type: str = "click",
     ):
         """Click element
 
@@ -793,34 +794,29 @@ class JavaAccessBridge:
         :param index: target element index if multiple are returned
         :param action: call click action on element (default), or use coordinates
         :param timeout: timeout in seconds to find element
+        :param click_type: default `click`, see `RPA.Desktop` for different
+         click options
         """
+        element = None
         if isinstance(locator, str):
-            interval = float(0.2)
-            end_time = time.time() + float(timeout)
-            while time.time() <= end_time:
-                start = time.time()
-                elements = self._find_elements(locator)
-                if len(elements) > 0:
-                    break
-                duration = time.time() - start
-                if duration < interval:
-                    time.sleep(interval - duration)
-
+            elements = self.wait_until_element_exists(locator, timeout)
             if len(elements) < (index + 1):
+                # print element tree when result is not expected
+                self.print_element_tree()
                 raise ElementNotFound(
                     "Locator '%s' matched only %s elements" % (locator, len(elements))
                 )
-            matching = elements[index]
+            element = elements[index]
         else:
-            matching = locator
+            element = locator
         try:
             if action:
-                self.logger.info("Element click action type:%s", type(matching))
-                matching.do_action("click")
+                self.logger.info("Element click action type:%s", type(element))
+                element.do_action("click")
             else:
-                self._click_element_middle(matching)
+                self._click_element_middle(element, click_type)
         except NotImplementedError:
-            self._click_element_middle(matching)
+            self._click_element_middle(element, click_type)
 
     @keyword
     def call_element_action(self, locator: str, action: str):
@@ -1138,3 +1134,23 @@ class JavaAccessBridge:
         self.application_refresh()
 
         return self.pid
+
+    @keyword
+    def wait_until_element_exists(self, locator: str, timeout: int = 10):
+        elements = None
+        end_time = time.time() + float(timeout)
+        while time.time() <= end_time:
+            start_time = time.time()
+            elements = self._find_elements(locator)
+            if len(elements) > 0:
+                break
+            self.application_refresh()
+            # sleep at least a second per iteration
+            duration = time.time() - start_time
+            if duration < 1.0:
+                time.sleep(1.0 - duration)
+        if elements is None or len(elements) == 0:
+            # print element tree when result is not expected
+            self.print_element_tree()
+            raise ElementNotFound("Locator '%s' did not match any elements" % locator)
+        return elements
