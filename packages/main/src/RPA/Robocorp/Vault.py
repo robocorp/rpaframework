e0cc9e828c17e49540e699856a2fd4b209cbfa45
Mikko Korpela <mikko@robocorp.com>
2023-11-13 16:28:26 +0200

    amazing

--- packages/main/src/RPA/Robocorp/Vault.py
+++ packages/main/src/RPA/Robocorp/Vault.py
@@ -202,15 +202,23 @@ def _raise_invalid_configuration():
         missing_vars_str = ", ".join(missing_env_vars)
 
         error_message = (
-            f"Configuration Error: Missing required environment variable(s): {missing_vars_str}.\n\n"
+            f"Configuration Error: Missing required environment variable(s):"
+            " {missing_vars_str}.\n\n"
             "To connect to the Robocorp Control Room, these variables are essential. "
-            "Please verify your configuration to ensure that all required environment variables are set.\n\n"
-            "For local runs, configure these variables in your 'devdata/env.json' file.\n\n"
-            "Consult the 'Configure file vault support' section in the documentation for step-by-step setup instructions:\n"
-            "https://robocorp.com/docs/development-guide/variables-and-secrets/vault\n\n"
-            "When running from the Control Room, these variables should be configured automatically.\n"
-            "If running with Robocorp Visual Studio Code extension, ensure that the project is linked to Control Room:\n"
-            "https://robocorp.com/docs/developer-tools/visual-studio-code/extension-features#linking-to-control-room"
+            "Please verify your configuration to ensure that all required environment"
+            " variables are set.\n\n"
+            "For local runs, configure these variables in your 'devdata/env.json'"
+            " file.\n\n"
+            "Consult the 'Configure file vault support' section in the documentation"
+            " for step-by-step setup instructions:\n"
+            "https://robocorp.com/docs/development-guide/"
+            "variables-and-secrets/vault\n\n"
+            "When running from the Control Room, these variables should be "
+            "configured automatically.\n"
+            "If running with Robocorp Visual Studio Code extension, ensure that"
+            " the project is linked to Control Room:\n"
+            "https://robocorp.com/docs/developer-tools/visual-studio-code/"
+            "extension-features#linking-to-control-room"
         )
 
         raise KeyError(error_message)
