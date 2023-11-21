666648a9c8cf4344dc56fb24e22936c02265d2cc
Mika Hänninen <mika.hanninen@gmail.com>
2023-11-21 15:06:34 +0200

    Update helpers.py

--- packages/core/src/RPA/core/windows/helpers.py
+++ packages/core/src/RPA/core/windows/helpers.py
@@ -11,6 +11,9 @@ def get_process_list(get_process_info: bool = True) -> Dict:
     """Get process list.
 
     Returns dictionary mapping process id to process name
+
+    :param get_process_info: If True, returns dictionary mapping
+     process id to process name. On False, returns empty dictionary.
     """
     if get_process_info:
         return {proc.pid: proc.name() for proc in psutil.process_iter()}
