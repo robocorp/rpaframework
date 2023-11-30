82eed4892e48d912620d4cd3e0abc86934512917
Kylie McNaughton <kylieblake@robocorp.com>
2023-11-30 11:06:18 -0500

    Update bootstrap functions to allow comma-separated version constraints
    Now creates a tempfile excluding `invoke` and `pip` requirements to allow install.

--- invocations/bootstrap.py
+++ invocations/bootstrap.py
@@ -5,8 +5,20 @@ from pathlib import Path
 
 import pkg_resources
 from invoke import task
+import tempfile
 
 REQUIREMENTS = (Path(__file__).parent / "requirements.txt").resolve()
+
+
+def create_temp_requirements():
+    """Creates a temporary requirements file without 'invoke' and 'pip'."""
+    with open(REQUIREMENTS) as reqs:
+        temp_reqs = tempfile.NamedTemporaryFile(mode="w", delete=False)
+        for line in reqs:
+            if "invoke" not in line.lower() and "pip" not in line.lower():
+                temp_reqs.write(line)
+        temp_reqs.close()
+        return temp_reqs.name
 
 
 @task(
@@ -14,17 +26,15 @@ REQUIREMENTS = (Path(__file__).parent / "requirements.txt").resolve()
 )
 def install_invocations(ctx, force=False):
     """Installs invocation dependencies directly into the system Python
-    installation used to invoke this command via Pip and the requirements.txt
-    maintained in this package.
+    installation used to invoke this command via Pip and the temporary requirements.txt
+    file without 'invoke' and 'pip'.
     """
-    with open(REQUIREMENTS) as reqs:
-        to_install = [
-            line.strip("\n") for line in reqs if not re.match(r"invoke|pip", line)
-        ]
-        ctx.run(
-            f"pip install {' '.join(to_install)}"
-            f"{' --force-reinstall' if force else ''}"
-        )
+    temp_requirements = create_temp_requirements()
+    ctx.run(
+        f"pip install -r {temp_requirements}" f"{' --force-reinstall' if force else ''}"
+    )
+    # Delete the temporary requirements file
+    Path(temp_requirements).unlink(missing_ok=True)
 
 
 def check_dependancy_versions() -> bool:
